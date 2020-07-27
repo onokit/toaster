2d069881cbd440eba7365374126459b3081cef04
kumarstack55 <kumarstack55@gmail.com>
2020-07-27 23:05:46 +0900

    Add ExpirationDurationMinutes

--- toaster.ps1
+++ toaster.ps1
@@ -1,3 +1,9 @@
+﻿[CmdletBinding()]
+param(
+    [parameter(mandatory)][String]$XmlPath,
+    [int]$ExpirationDurationMinutes = $null
+)
+
 # Stop processing when any error occured
 $ErrorActionPreference = "Stop"
 
@@ -6,11 +12,14 @@ $ErrorActionPreference = "Stop"
 [Windows.UI.Notifications.ToastNotification, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
 
 # Read toast template xml as [Windows.Data.Xml.Dom.XmlDocument] object
-$template = Get-Content $Args[0]
+$template = Get-Content $XmlPath
 $xml = New-Object Windows.Data.Xml.Dom.XmlDocument
 $xml.LoadXml($template)
 
 # Toast notify
 $app = '{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'
 $toast = New-Object Windows.UI.Notifications.ToastNotification $xml
+if ($ExpirationDurationMinutes -ne $null) {
+    $toast.ExpirationTime = $(Get-Date).AddMinutes($ExpirationDurationMinutes)
+}
 [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($app).Show($toast)
